f127287dd06e475cdb545f82b8c85bfa5953c616
Andrew Connally <ajconnally93@gmail.com>
2023-02-20 21:03:25 -0600

    indexjs file in models with exports

---
+++ models/index.js
@@ -0,0 +1,5 @@
+// will export Reaction differently since it's set up a little differently
+
+const User = require('./User');
+const Thought = require('./Thought');
+module.exports = { User, Thought };
